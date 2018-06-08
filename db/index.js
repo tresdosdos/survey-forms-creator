48ba2e5d57eda46f481c60615ec9d1c0f019034b
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-08 11:51:25 +0300

    initial commit

--- db/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-mongoose.connect('mongodb://tresdosdos:strelok11@ds229415.mlab.com:29415/survey-forms', {
-    promiseLibrary: global.Promise
-});
-
-module.exports = mongoose;
