81268d912059435b4581e16706426d9d4c6eb9f1
tresdosdos <vitaliystrelok8@gmail.com>
2018-06-08 12:27:26 +0300

    z

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://tresdosdos:strelok11@ds229415.mlab.com:29415/forms', {
+mongoose.connect('mongodb://tresdosdos:strelok11@ds229415.mlab.com:29415/survey-forms', {
     promiseLibrary: global.Promise
 });
 
